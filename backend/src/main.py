93f2f0cb63d267da5d05b5759152efc73b52b556
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-06-28 11:01:20 +0000

    use dotenv

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+load_dotenv(verbose=True)
+
+doDebug = True if os.getenv("DEBUG").lower() == "true" else False
 
 @app.route("/")
 def hello_world():
     return "<p>Hello world!</p>"
+
+if __name__=="__main__":
+    app.run(debug=doDebug, host='0.0.0.0', port=5000)
