17e13ba7999dc1ab99bc2dd12d7ba7fdc7389a25
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-06-29 16:03:11 +0000

    rename some code

--- backend/app.py
+++ backend/app.py
@@ -2,21 +2,17 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask
-from sound2text import callWhisper
 
-from app_route import translate_module
+from route.gpt_route import chatGPT_module
+from route.whisper_route import whisper_module
 
 app = Flask(__name__)
 load_dotenv(verbose=True)
 
 doDebug = True if os.getenv("DEBUG").lower() == "true" else False
 
-@app.route("/")
-def whisper():
-    messege = callWhisper("小森めと.mp4")
-    return messege
-
-app.register_blueprint(translate_module)
+app.register_blueprint(chatGPT_module)
+app.register_blueprint(whisper_module)
 
 if __name__=="__main__":
     app.run(debug=doDebug, host='0.0.0.0', port=5000)
--- backend/utils/listener.py
+++ backend/utils/listener.py
@@ -1,7 +1,7 @@
+import os
+
 import openai
 from dotenv import load_dotenv
-
-import os
 
 load_dotenv(verbose=True)
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -11,9 +11,8 @@ audiopath = os.path.join(basedir, "../audio/")
 
 def callWhisper(fname:str) -> str:
     fpath = os.path.join(audiopath, fname)
-
-    audio_file= open(fpath, "rb")
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    with open(fpath, "rb") as audio_file:
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
     return transcript.text
 
 if __name__=="__main__":
