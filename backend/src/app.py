93374e4f498c9f9f79288ea8ab9b9d4afc16d4f3
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-06-30 19:57:36 +0000

    urlに/liscript/apiのprefixを追加

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, Blueprint
 
 from route.gpt_route import chatGPT_module
 from route.whisper_route import whisper_module
@@ -11,8 +11,12 @@ load_dotenv(verbose=True)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
-app.register_blueprint(chatGPT_module)
-app.register_blueprint(whisper_module)
+api_route_blueprint = Blueprint("api_route", __name__, url_prefix="/liscript/api/")
+
+api_route_blueprint.register_blueprint(chatGPT_module)
+api_route_blueprint.register_blueprint(whisper_module)
+
+app.register_blueprint(api_route_blueprint)
 
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
