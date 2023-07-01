49295ccf38c75b1b40d09fa2a39d0f7fb214e6fe
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-07-01 17:57:32 +0000

    Fileの保存を実装

--- frontend/components/file-picker.tsx
+++ frontend/components/file-picker.tsx
@@ -1,17 +1,21 @@
 import { useCallback } from "react"
 import { DropEvent, FileRejection, useDropzone } from "react-dropzone"
+import { useSetFile } from "@/store"
 
 export type FilePickerProps = Omit<React.HTMLAttributes<HTMLDivElement>, "children"> & {
 
 }
 
 export const FilePicker = (props: FilePickerProps) => {
+  const setFile = useSetFile()
+
   const onDrop = useCallback(((
     acceptedFiles: File[],
     fileRejections: FileRejection[],
     event: DropEvent
   ) => {
     console.log(acceptedFiles, fileRejections, event)
+    setFile(acceptedFiles[0] ?? null)
   }), [])
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
