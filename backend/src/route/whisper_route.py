05286c62ea17b87431ea2cccf5b3d21a46ac36d8
Rei1449 <kiriya4673@gmail.com>
2023-07-01 09:34:10 +0000

    add process to reduce audio data

--- backend/src/route/whisper_route.py
+++ backend/src/route/whisper_route.py
@@ -15,7 +15,7 @@ load_dotenv(verbose=True)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 UPLOAD_FOLDER = os.path.join(basedir, "../audio/")
-ALLOWED_EXTENSIONS = {'mp3',"m4a"}
+ALLOWED_EXTENSIONS = {'mp3',"m4a","mp4"}
 
 MAX_AUDIO_SIZE = 26214400
 
@@ -23,10 +23,17 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def reduce_audio_size(before_modify_filename):
+def reduce_audio_size(before_modify_filename,before_export_filepath):
     fpath = os.path.join(UPLOAD_FOLDER, before_modify_filename)
     sourceAudio = AudioSegment.from_file(fpath)
-    return sourceAudio.frame_rate
+    # フレームレートを低下させ、データ量を減らす
+    audio = sourceAudio.set_frame_rate(16000)
+    # 全てのファイルをwavファイルに変換し、保存
+    output_path = os.path.join(UPLOAD_FOLDER, "output.wav")
+    audio.export(output_path, format="wav")
+    # サイズを小さくする前のファイルを削除
+    os.remove(before_export_filepath)
+    return output_path,"output.wav"
 
 @whisper_module.route("/convert", methods=['GET', 'POST'])
 def convert_audio_text():
@@ -58,21 +65,24 @@ def convert_audio_text():
                 return redirect(request.url)
             
             audio_size = os.path.getsize(export_filepath)
-            if audio_size < MAX_AUDIO_SIZE:
-                a = reduce_audio_size(export_filename)
-                return str(a)
+            if audio_size > MAX_AUDIO_SIZE:
+                # 音声データを小さくし、ファイル名とファイルパスを更新
+                export_filepath,export_filename = reduce_audio_size(export_filename,export_filepath)
+                small_audio_size = os.path.getsize(export_filepath)
 
+                if small_audio_size > MAX_AUDIO_SIZE:
+                    flash('This audio data is too large.')
+                    return redirect(request.url)
                 
-            
-            # # 以下はWhisperの処理
-            # try:
-            #     messege = callWhisper(export_filename)
-            # except:
-            #     flash('An error occurred during speech recognition')
-            #     return redirect(request.url)
+            # 以下はWhisperの処理
+            try:
+                messege = callWhisper(export_filename)
+            except:
+                flash('An error occurred during speech recognition')
+                return redirect(request.url)
 
-            # os.remove(export_filepath)
-            # return jsonify(messege)
+            os.remove(export_filepath)
+            return jsonify(messege)
 
     return '''
     <!doctype html>
