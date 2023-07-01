aad354fa0774f739827d6fd515262b1187722983
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-07-01 04:35:55 +0000

    react-dropzoneでのファイルのアップロードを実装

--- frontend/components/main.tsx
+++ frontend/components/main.tsx
@@ -1,14 +1,25 @@
 import { Box, Input, Button } from "@chakra-ui/react";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { css } from "@emotion/react";
 
+import { DropEvent, FileRejection, useDropzone } from "react-dropzone";
+
 export function Main() {
-  const [leftInputValue, setLeftInputValue] = useState("")
   const [rightInputValue, setRightInputValue] = useState("")
 
   const handleClickCopyButton = () => {
       navigator.clipboard.writeText(rightInputValue);
     }
+  
+  const onDrop = useCallback(((
+    acceptedFiles: File[],
+    fileRejections: FileRejection[],
+    event: DropEvent
+  ) => {
+    console.log(acceptedFiles, fileRejections, event)
+  }), [])
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
   
   return (
     <Box
@@ -24,25 +35,27 @@ export function Main() {
         }
       `}
     >
-      
-      <Input
-        css={css`
-          width: 45%;
-          height: 80%;
-          border-radius: 7px;
-          border: 1px solid #000;
-          background: #FFF;
-          box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25), 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-          margin-right: 3%;
-          @media (max-width: 768px) {
-           margin-right:0%;
-           width:80%;
-           margin:30px;
-          }
-        `}
-        value={leftInputValue}
-        onChange={e => setLeftInputValue(e.target.value)}
-      />
+      <div {...getRootProps()} css={css`
+        width: 45%;
+        height: 80%;
+        border-radius: 7px;
+        border: 1px solid #000;
+        background: #FFF;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25), 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        margin-right: 3%;
+        @media (max-width: 768px) {
+          margin-right:0%;
+          width:80%;
+          margin:30px;
+        }
+      `}>
+        <input {...getInputProps()}/>
+        {
+          isDragActive ?
+            <p>ファイルをドロップしてください。</p> :
+            <p>ドラック & ドロップ または クリックをしてファイルをアップロードしてください。</p>
+        }
+      </div>
       <Box css={css`
             position:relative;
             width:45%;
