4eab9442b582a74b495f7a77b31605e36ffb8bd9
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2023-07-01 22:07:06 +0000

    翻訳時にはみ出さないように修正

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -7,12 +7,7 @@ import { FileTextViewer, GeneratedTextViewer } from "./file-text-viewer";
 
 export function Main() {
   const file = useFile()
-  const [rightInputValue, setRightInputValue] = useState("")
   const setGeneratedViewerMode = useSetGeneratedViewerMode()
-
-  const handleClickCopyButton = () => {
-      navigator.clipboard.writeText(rightInputValue);
-    }
    
   return (
     <Box
@@ -63,41 +58,24 @@ export function Main() {
           翻訳
         </Button>
       </Box>
-      <Box css={css`
-        position:relative;
-        width:40%;
-        height:80%;
+      <div css={css`
+        width: 40%;
+        height: 80%;
+        border-radius: 7px;
+        border: 1px solid #000;
+        background: #FFF;
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25), 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        overflow: scroll;
         @media (max-width: 768px) {
           margin-right:0%;
           width:80%;
-          margin:10px;
+          margin:30px;
         }
-     `}>
-        <Box
-          css={css`
-            width: 100%;
-            height: 100%;
-            border-radius: 7px;
-            border: 1px solid #000;
-            background: #FFF;
-            box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25), 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-          }
-          `}
-        >
-          <Suspense fallback="loading">
-            <GeneratedTextViewer />
-          </Suspense>
-        </Box>    
-        <Button
-          position="absolute"
-          right="10px"
-          bottom="10px"
-          transform="translateY(-50%)"
-          onClick={handleClickCopyButton}
-        >
-          Copy
-        </Button>
-      </Box>
+      `}>
+        <Suspense fallback="loading">
+          <GeneratedTextViewer />
+        </Suspense>
+      </div>
     </Box>
   );
 }
