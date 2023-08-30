3a9357d58e00e7a8c30dc1f09923ba0e8f766f2b
Rei1449 <kiriya4673@gmail.com>
2023-08-30 21:57:36 +0900

    Increased maximum audio file size

--- backend/src/route/whisper_route.py
+++ backend/src/route/whisper_route.py
@@ -23,17 +23,37 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def reduce_audio_size(before_modify_filename,before_export_filepath):
+def split_and_reduce_audio(source_audio, chunk_length_ms):
+    chunks = []
+    total_duration = len(source_audio)
+    chunk_count = total_duration // chunk_length_ms
+
+    for i in range(chunk_count):
+        start_time = i * chunk_length_ms
+        end_time = (i + 1) * chunk_length_ms
+        chunk = source_audio[start_time:end_time]
+        chunks.append(chunk)
+
+    return chunks
+
+def reduce_audio_size(before_modify_filename, before_export_filepath):
     fpath = os.path.join(UPLOAD_FOLDER, before_modify_filename)
-    sourceAudio = AudioSegment.from_file(fpath)
-    # フレームレートを低下させ、データ量を減らす
-    audio = sourceAudio.set_frame_rate(16000)
-    # 全てのファイルをwavファイルに変換し、保存
-    output_path = os.path.join(UPLOAD_FOLDER, "output.wav")
-    audio.export(output_path, format="wav")
-    # サイズを小さくする前のファイルを削除
-    os.remove(before_export_filepath)
-    return output_path,"output.wav"
+    source_audio = AudioSegment.from_file(fpath)
+
+    # 分割とリサンプリング
+    chunk_length_ms = 60000  # 60秒毎に分割
+    audio_chunks = split_and_reduce_audio(source_audio, chunk_length_ms)
+
+    # 各チャンクを処理
+    output_chunks = []
+    for i, chunk in enumerate(audio_chunks):
+        #chunk = chunk.set_frame_rate(16000)
+        chunk_filename = f"chunk_{i + 1}.wav"
+        chunk.export(os.path.join(UPLOAD_FOLDER, chunk_filename), format="wav")
+        output_chunks.append(chunk_filename)
+
+    return output_chunks
+
 
 @whisper_module.route("/convert", methods=['GET', 'POST'])
 def convert_audio_text():
@@ -65,25 +85,43 @@ def convert_audio_text():
             
             audio_size = os.path.getsize(export_filepath)
             if audio_size > MAX_AUDIO_SIZE:
-                # 音声データを小さくし、ファイル名とファイルパスを更新
-                export_filepath,export_filename = reduce_audio_size(export_filename,export_filepath)
-                small_audio_size = os.path.getsize(export_filepath)
+                # 音声データを分割してリサンプリング
+                output_chunk_filenames = reduce_audio_size(export_filename, export_filepath)
 
-                if small_audio_size > MAX_AUDIO_SIZE:
-                    messege = 'This audio data is too large.'
+                if len(output_chunk_filenames) == 0:
+                    messege = 'Failed to process audio.'
                     os.remove(export_filepath)
-                    return jsonify(messege), 400                    
-                
-            # 以下はWhisperの処理
-            try:
-                messege = callWhisper(export_filename)
-            except:
-                messege = 'An error occurred during speech recognition'
-                os.remove(export_filepath)
-                return jsonify(messege), 400
+                    return jsonify(messege), 400
 
-            os.remove(export_filepath)
-            return jsonify(messege), 200
+                # 分割されたチャンクを個別にWhisperに送信して処理
+                messages = []
+                for chunk_filename in output_chunk_filenames:
+                    try:
+                        chunk_messege = callWhisper(chunk_filename)
+                        messages.append(chunk_messege)
+                    except:
+                        messages.append('An error occurred during speech recognition')
+                        #os.remove(os.path.join(UPLOAD_FOLDER, chunk_filename))
+
+                # 分割されたファイルを削除
+                for chunk_filename in output_chunk_filenames:
+                    os.remove(os.path.join(UPLOAD_FOLDER, chunk_filename))
+
+                combined_message = ''.join(messages)
+                messege = [combined_message]
+                os.remove(export_filepath)
+                return jsonify(messege),200
+            else:
+                try:
+                    messege = callWhisper(export_filename)
+                except:
+                    messege = 'An error occurred during speech recognition'
+                    os.remove(export_filepath)
+                    return jsonify(messege), 400
+                    
+                os.remove(export_filepath)
+                return jsonify(messege), 200
+
 
     return '''
     <!doctype html>
@@ -94,6 +132,78 @@ def convert_audio_text():
       <input type=submit value=Upload>
     </form>
     '''
+
+# def reduce_audio_size(before_modify_filename,before_export_filepath):
+#     fpath = os.path.join(UPLOAD_FOLDER, before_modify_filename)
+#     sourceAudio = AudioSegment.from_file(fpath)
+#     # フレームレートを低下させ、データ量を減らす
+#     audio = sourceAudio.set_frame_rate(16000)
+#     # 全てのファイルをwavファイルに変換し、保存
+#     output_path = os.path.join(UPLOAD_FOLDER, "output.wav")
+#     audio.export(output_path, format="wav")
+#     # サイズを小さくする前のファイルを削除
+#     os.remove(before_export_filepath)
+#     return output_path,"output.wav"
+
+# @whisper_module.route("/convert", methods=['GET', 'POST'])
+# def convert_audio_text():
+#     if request.method == 'POST':
+
+#         if 'file' not in request.files:
+#             messege = 'No file part.'
+#             return jsonify(messege), 400
+#         recieved_file = request.files['file']
+#         recieved_filename = recieved_file.filename
+
+#         if recieved_filename == '':
+#             messege = 'No selected file.'
+#             return jsonify(messege), 400
+        
+#         if recieved_file and allowed_file(recieved_filename):
+#             hashing = lambda fname: str(uuid.uuid5(uuid.NAMESPACE_DNS, fname))
+
+#             filename, extention = recieved_filename.split(".")
+#             export_filename = secure_filename(hashing(filename) + F".{extention}")
+            
+#             try:
+#                 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+#                 export_filepath = os.path.join(UPLOAD_FOLDER, export_filename)
+#                 recieved_file.save(export_filepath)
+#             except:
+#                 messege = 'An error occurred while the file was being saved or transferred.'
+#                 return jsonify(messege), 400
+            
+#             audio_size = os.path.getsize(export_filepath)
+#             if audio_size > MAX_AUDIO_SIZE:
+#                 # 音声データを小さくし、ファイル名とファイルパスを更新
+#                 export_filepath,export_filename = reduce_audio_size(export_filename,export_filepath)
+#                 small_audio_size = os.path.getsize(export_filepath)
+
+#                 if small_audio_size > MAX_AUDIO_SIZE:
+#                     messege = 'This audio data is too large.'
+#                     os.remove(export_filepath)
+#                     return jsonify(messege), 400                    
+                
+#             # 以下はWhisperの処理
+#             try:
+#                 messege = callWhisper(export_filename)
+#             except:
+#                 messege = 'An error occurred during speech recognition'
+#                 os.remove(export_filepath)
+#                 return jsonify(messege), 400
+
+#             os.remove(export_filepath)
+#             return jsonify(messege), 200
+
+#     return '''
+#     <!doctype html>
+#     <title>Upload new File</title>
+#     <h1>Upload new File</h1>
+#     <form method=post enctype=multipart/form-data>
+#       <input type=file name=file>
+#       <input type=submit value=Upload>
+#     </form>
+#     '''
 
 # 参考
 # https://qiita.com/fghyuhi/items/d42ce8cb1f5de5280ac5
