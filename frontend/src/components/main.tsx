24dbd237ccbce2fabc9564e94a81c2202df4a64d
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2023-08-31 05:57:23 +0000

    add selectbox

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -2,6 +2,7 @@ import { Box, Input, Button, Spinner } from "@chakra-ui/react";
 import { Suspense, useState } from "react";
 import { css } from "@emotion/react";
 import { FilePicker } from "./file-picker";
+import { SelectBox } from "./select-language-box";
 import { useFile, useSetGeneratedViewerMode } from "@/store";
 import { FileTextViewer, GeneratedTextViewer } from "./file-text-viewer";
 
@@ -61,70 +62,76 @@ export function Main() {
    
   return (
     <Box
-      flexDirection={{ base: "column", md: "row" }}
-      height={{ base: "120vh", md: "86vh" }}
       css={css`
-        display: flex;
-        justify-content: center;
-        align-items: center;
         background-image : url(/liscript/img/bg-image.png);
       `}
     >
-      {file === null ? (
-        <MainBox>
-          <FilePicker/>
-        </MainBox>
-      ) : (
-        <Suspense fallback={
-          <MainBox>
-            <SpinnerBox />
-          </MainBox>
-        }>
-          <MainBox scroll>
-            <FileTextViewer/>
-          </MainBox>
-        </Suspense>
-      )}
+      <SelectBox />
       <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection={{ base: "row", md: "column" }}
+        flexDirection={{ base: "column", md: "row" }}
+        height={{ base: "120vh", md: "86vh" }}
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        `}
       >
-        <Button
-          colorScheme="green"
-          size="lg" 
-          margin="4"
-          boxShadow="dark-lg"
-          onClick={() => {
-            setGeneratedViewerMode("summarize")
-          }}
-        >
-          要約
-        </Button>
-        <Button
-          colorScheme="blue" 
-          size="lg" 
-          margin="4"
-          boxShadow="dark-lg"
-          onClick={() => {
-            setGeneratedViewerMode("translate")
-          }}
-        >
-          翻訳
-        </Button>
-      </Box>
-      {(
-        <Suspense fallback={
+        {file === null ? (
           <MainBox>
-            <SpinnerBox />
+            <FilePicker/>
           </MainBox>
-        }>
-          <MainBox scroll>
-            <GeneratedTextViewer />
-          </MainBox>
-        </Suspense>
-      )}
+        ) : (
+          <Suspense fallback={
+            <MainBox>
+              <SpinnerBox />
+            </MainBox>
+          }>
+            <MainBox scroll>
+              <FileTextViewer/>
+            </MainBox>
+          </Suspense>
+        )}
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection={{ base: "row", md: "column" }}
+        >
+          <Button
+            colorScheme="green"
+            size="lg" 
+            margin="4"
+            boxShadow="dark-lg"
+            onClick={() => {
+              setGeneratedViewerMode("summarize")
+            }}
+          >
+            要約
+          </Button>
+          <Button
+            colorScheme="blue" 
+            size="lg" 
+            margin="4"
+            boxShadow="dark-lg"
+            onClick={() => {
+              setGeneratedViewerMode("translate")
+            }}
+          >
+            翻訳
+          </Button>
+        </Box>
+        {(
+          <Suspense fallback={
+            <MainBox>
+              <SpinnerBox />
+            </MainBox>
+          }>
+            <MainBox scroll>
+              <GeneratedTextViewer />
+            </MainBox>
+          </Suspense>
+        )}
+      </Box>
     </Box>
   );
 }
